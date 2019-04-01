16c452ac5d3822f198d7034760bf3b43c9c4cded
KyeongsangNa <hellraris@gmail.com>
2019-04-01 23:02:02 +0900

    選択肢のタイプ変更

--- client/src/components/viewer/TestViewer.js
+++ client/src/components/viewer/TestViewer.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const styles = theme => ({
-    root: {
+    questionViewer: {
         width: '80%',
         paddingTop: theme.spacing.unit * 2,
         paddingBottom: theme.spacing.unit * 2,
@@ -23,7 +25,9 @@ class TestViewer extends Component {
         this.state = {
             nowQuestion: '',
             questions: '',
-            questionOrder: 0
+            questionOrder: 0,
+            selected: -1,
+            markingList:[]
         }
     }
 
@@ -62,39 +66,51 @@ class TestViewer extends Component {
         })
     }
 
+    handleSelectionClick = (index) => {
+        this.setState({
+            ...this.state,
+            selected: index
+        })
+    }
+
     render() {
         const { classes } = this.props;
 
         return (
-            <div>
+            <div className={classes.questionViewer}>
                 {this.state.nowQuestion ?
                     <div>
-                        <Paper className={classes.root} elevation={1}>
+                        <Paper elevation={1}>
                             <Typography component="p">
                                 {this.state.nowQuestion.question.script}
                             </Typography>
                         </Paper>
                         <div>
-                            {
-                                this.state.nowQuestion.question.selections.map((s, index) => {
-                                    return (
-                                        <div className={classes.selection} key={index}>
-                                            <TextField
-                                                className={classes.textField}
-                                                margin="normal"
-                                                multiline
-                                                value={s.selection}
-                                            />
-                                        </div>
-                                    )
-                                })
-                            }
+                            <List component="nav">
+                                {
+                                    this.state.nowQuestion.question.selections.map((s, index) => {
+                                        return (
+                                                <ListItem
+                                                    button
+                                                    selected={this.state.selected === index}
+                                                    onClick={() => this.handleSelectionClick(index)}
+                                                >
+                                                    <ListItemText>{s.selection}</ListItemText>
+                                                </ListItem>
+                                        )
+                                    })
+                                }
+                            </List>
                         </div>
                         <div>
-                            <Button onClick={() => this.hendleQuestionOrder("prev")}>PREV</Button>
-                            <Button onClick={() => this.hendleQuestionOrder("next")}>NEXT</Button>
+                            {this.state.questionOrder === 0 ? '' : <Button onClick={() => this.hendleQuestionOrder("prev")}>PREV</Button>}
+                            {this.state.questionOrder === this.state.questions.length - 1 ?
+                                <Button>SEND</Button>
+                                :
+                                <Button onClick={() => this.hendleQuestionOrder("next")}>NEXT</Button>
+                            }
                         </div>
-                    </div>
+                    </div >
                     :
                     ''
                 }
